a1cf773f717ec2b608b21f1470a33c80cefe7a44
Your Name <you@example.com>
2026-01-05 23:12:28 +0200

    - added appTextStyles.

---
+++ lib/core/theming/app_text_styles.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:islami/core/theming/app_colors.dart';
+
+abstract final class AppTextStyles {
+  static final font24GoldBold = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    color: AppColors.primary,
+  );
+
+  static final font20GoldBold = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+    color: AppColors.primary,
+  );
+
+  static final font16GoldBold = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+    color: AppColors.primary,
+  );
+}
