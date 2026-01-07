6a45ecdcd27cd66d85db8323dfaa4c3ac78be981
Your Name <you@example.com>
2026-01-07 23:49:24 +0200

    - added app routes.

---
+++ lib/core/routing/app_routes.dart
@@ -0,0 +1,7 @@
+class AppRoutes {
+  static const String quran = '/home';
+  static const String hadith = '/hadith';
+  static const String sebha = '/hadith';
+  static const String radio = '/radio';
+  static const String time = '/time';
+}
