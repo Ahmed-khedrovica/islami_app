3bda69c6e249116b36b8c374edaa5c4edb338b90
Your Name <you@example.com>
2026-01-16 21:30:25 +0200

    - added flutter_bloc. - added font style. - added some assets. - finish sebha screen.

--- lib/core/routing/go_router_config.dart
+++ lib/core/routing/go_router_config.dart
@@ -3,6 +3,7 @@ import 'package:go_router/go_router.dart';
 import 'package:islami/core/routing/app_routes.dart';
 import 'package:islami/features/home/ui/home_screen.dart';
 import 'package:islami/features/onboarding/ui/onboarding.dart';
+import 'package:islami/features/sebha/ui/sebha_screen.dart';
 
 import '../app/widgets/app_bottom_navigation.dart';
 
@@ -33,7 +34,7 @@ final GoRouter appRouter = GoRouter(
         ),
         GoRoute(
           path: AppRoutes.sebha,
-          builder: (context, state) => Placeholder(),
+          builder: (context, state) => SebhaScreen(),
         ),
         GoRoute(
           path: AppRoutes.radio,
---
+++ lib/features/sebha/ui/widgets/sebha_body.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../core/theming/app_text_styles.dart';
+import '../../logic/sebha_cubit.dart';
+
+class SebhaBody extends StatelessWidget {
+  const SebhaBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => SebhaCubit(),
+      child: Stack(
+        children: [
+          Image.asset('assets/images/sebha_body_bg.png'),
+          Positioned.fill(
+            child: BlocBuilder<SebhaCubit, SebhaState>(
+              builder: (context, state) {
+                return InkWell(
+                  onTap: (){
+                    context.read<SebhaCubit>().incrementCounter();
+                  },
+                  child: Column(
+                      mainAxisAlignment: .center,
+                      children: [
+                        Text(
+                            'سبحان الله',
+                            style: AppTextStyles.font36WhiteBold
+                        ),
+                        Text(
+                            state.counter.toString(),
+                            style: AppTextStyles.font36WhiteBold
+                        ),
+                      ]
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
